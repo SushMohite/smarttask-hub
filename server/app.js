389cc0efec169c186c73f493fe9b77148851d944
SushMohite <sushantmohite85030@gmail.com>
2026-05-06 23:22:38 +0530

    Initial project setup

---
+++ server/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("SmartTask Hub API running...");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running...");
+});
